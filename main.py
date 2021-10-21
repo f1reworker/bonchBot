c1e82cfd6a3250f86a304ee222217d7ade9df02b
fireworker <daniil.shalin2406@mail.ru>
2021-10-22 01:46:49 +0300

    for kk

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from selenium import webdriver
 import schedule
 import time
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -34,14 +35,17 @@ def getSchedule():
                 sch = WebDriverWait(driver, 1).until(EC.visibility_of_element_located((By.LINK_TEXT, "Расписание")))
             finally:
                 sch.click()
-                try: 
-                    elements = WebDriverWait(driver, 1).until(EC.visibility_of_elements_located((By.PARTIAL_LINK_TEXT, "Кнопка")))
+                try:
+                    time.sleep(2)
+                    elements = driver.find_elements_by_partial_link_text("Кнопка")
                 finally:
                     for elements in elements:
                         elementsArr.append(elements.text.split(" ")[3].replace(".", ""))
                     elementsArr = set(elementsArr)
+                    elementsArr = list(elementsArr)
                     driver.quit()
                     clickButton(elementsArr)
+                    print(elementsArr)
 
 
 def clickButton(elementsArr):
@@ -69,13 +73,14 @@ def click():
                 sch = WebDriverWait(driver, 1).until(EC.visibility_of_element_located((By.LINK_TEXT, "Расписание")))
             finally:
                 sch.click()
-                try:
-                    button = WebDriverWait(driver, 1).until(
-                    EC.visibility_of_element_located((By.PARTIAL_LINK_TEXT, "Начать"))
-                )
-                finally:
-                    button.click()
-                    driver.quit()
+                i = 0
+                while i<=20:
+                    try:
+                        driver.find_element_by_partial_link_text("Начать").click()
+                    except NoSuchElementException:
+                        time.sleep(300)
+                        i+=5
+                        driver.find_element_by_partial_link_text("Обновить").click()
 
 def testSch():
     driver = webdriver.Chrome(executable_path=pathToExe,options=chrome_options)
@@ -95,14 +100,17 @@ def testSch():
                 sch = WebDriverWait(driver, 1).until(EC.visibility_of_element_located((By.LINK_TEXT, "Расписание")))
             finally:
                 sch.click()
+                i = 0
+                while i<=20:
+                    try:
+                        driver.find_element_by_partial_link_text("Начать").click()
+                    except NoSuchElementException:
+                        time.sleep(300)
+                        i+=5
+                        driver.find_element_by_partial_link_text("Обновить").click()
 
-def arr():
-    elementsArr = ["20:15:58", "20:15", "20:16"]
-    i = 0
-    for i in range(0, len(elementsArr)):
-        # TODO: разобраться с shedule
-        schedule.every().day.at(elementsArr[i]).do(testSch)
-schedule.every().day.at("8:00").do(getSchedule)
+
+schedule.every().day.at("07:00").do(getSchedule)
 while True:
     schedule.run_pending()
     time.sleep(1)
