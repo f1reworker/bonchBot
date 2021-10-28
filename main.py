5d48590c219c97a0f915eaa53e8769a069703322
fireworker <daniil.shalin2406@mail.ru>
2021-10-28 19:58:22 +0300

    test for more person

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 from selenium import webdriver
 import time
 from selenium.common.exceptions import NoSuchElementException, UnexpectedAlertPresentException
@@ -28,7 +29,7 @@ chrome_options.add_argument("--no-sandbox")
 
 
 
-def getSchedule(user):
+def findSchedule(user):
     elementsArr = []
     driver = webdriver.Chrome(service = s, options = chrome_options)
     driver.get(url)
@@ -58,36 +59,6 @@ def getSchedule(user):
                     return elementsArr
             driver.quit()
 
-
-def click():
-    driver = webdriver.Chrome(service = s, options = chrome_options)
-    driver.get(url)
-    try:
-        login = WebDriverWait(driver, 1).until(
-        EC.visibility_of_element_located((By.NAME, "users")))
-    finally:
-        #login.send_keys(datas["users"])
-        #driver.find_element(By.NAME, "parole").send_keys(datas["parole"])
-        driver.find_element(By.NAME, "logButton").click()
-        try:
-            button = WebDriverWait(driver, 1).until(EC.visibility_of_element_located((By.CLASS_NAME, "lm_item")))
-        finally:
-            button.click()
-            try:
-                sch = WebDriverWait(driver, 1).until(EC.visibility_of_element_located((By.LINK_TEXT, "Расписание")))
-            finally:
-                sch.click()
-                i = 0
-                while i<=30:
-                    try:
-                        driver.find_element(By.PARTIAL_LINK_TEXT, "Начать").click()
-                    except NoSuchElementException:
-                        time.sleep(300)
-                        i+=5
-                        driver.find_element(By.PARTIAL_LINK_TEXT, "начала от").click()
-                driver.quit()
-
-
 def checkAuth(loginUser, passwordUser):
     driver = webdriver.Chrome(service = s, options = chrome_options)
     driver.get(url)
@@ -113,17 +84,74 @@ def pushSchedule():
     for i in range(0, len(usersArr)):
         user_id = usersArr[i].key()
         user = db.child("Users").child(user_id).get().val()
-        timeSched = getSchedule(user)
+        timeSched = findSchedule(user)
         if timeSched!=None:
             for q in range (0, len(timeSched)):
                 db.child("Schedule").child(timeSched[q]).child(user_id).set(False)
+    getSchedule()
+def click(user, timeInt):
+    while len(user)!=0:
+        user = list(db.child("Schedule").child(timeInt).get().val().keys())
+        for i in range(0, len(user)):
+            thisUser = (db.child("Users").child(user[i]).get().val())
+            login = thisUser["login"]
+            password = thisUser["password"]
+            driver = webdriver.Chrome(service = s, options = chrome_options)
+            driver.get(url)
+            try:
+                loginArea = WebDriverWait(driver, 1).until(
+                EC.visibility_of_element_located((By.NAME, "users")))
+            finally:
+                loginArea.send_keys(login)
+                driver.find_element(By.NAME, "parole").send_keys(password)
+                driver.find_element(By.NAME, "logButton").click()
+                try:
+                    button = WebDriverWait(driver, 1).until(EC.visibility_of_element_located((By.CLASS_NAME, "lm_item")))
+                finally:
+                    button.click()
+                    try:
+                        sch = WebDriverWait(driver, 1).until(EC.visibility_of_element_located((By.LINK_TEXT, "Расписание")))
+                    finally:
+                        sch.click()
+                        try:
+                            driver.find_element(By.PARTIAL_LINK_TEXT, "Начать").click()
+                        except NoSuchElementException:
+                            driver.quit()
+                        else:
+                            db.child("Users").child(timeInt).child(user[i]).remove()
+                            driver.quit()
+    return schedule.CancelJob
+
 
 def removeSchedule():
     db.child("Schedule").remove()
 
-#! TODO: поменять время
-schedule.every().day.at("15:08").do(removeSchedule)
-schedule.every().day.at("15:09").do(pushSchedule)
+def getSchedule():
+    timeArr = list(db.child("Schedule").get().val().keys())
+    for i in range(0, len(timeArr)):
+        threadNumber = i%6
+        if threadNumber == 0:
+            thread0 = Thread(target = schedule.every().day.at(timeArr[i]).do(click, timeArr[i]))
+            thread0.start()
+        elif threadNumber == 1:
+            thread1 = Thread(target = schedule.every().day.at(timeArr[i]).do(click, timeArr[i]))
+            thread1.start()
+        elif threadNumber == 2:
+            thread2 = Thread(target = schedule.every().day.at(timeArr[i]).do(click, timeArr[i]))
+            thread2.start()
+        elif threadNumber == 3:
+            thread3 = Thread(target = schedule.every().day.at(timeArr[i]).do(click, timeArr[i]))
+            thread3.start()
+        elif threadNumber == 4:
+            thread4 = Thread(target = schedule.every().day.at(timeArr[i]).do(click, timeArr[i]))
+            thread4.start()
+        elif threadNumber == 5:
+            thread5 = Thread(target = schedule.every().day.at(timeArr[i]).do(click, timeArr[i]))
+            thread5.start()
+
+
+schedule.every().day.at("22:00").do(removeSchedule)
+schedule.every().day.at("22:10").do(pushSchedule)
 
 def runSchedule():
     while True:
